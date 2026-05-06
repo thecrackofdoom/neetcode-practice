38ecf9e113321ff13e54815a7b41576ec6a212fd
tmng <114478287+thecrackofdoom@users.noreply.github.com>
2026-05-06 10:59:43 +0800

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        premap = {i:[] for i in range(numCourses)}
+        indeg = [0] * numCourses
+
+        for course, pre in prerequisites:
+            premap[pre].append(course)
+            indeg[course] += 1
+        print(premap)
+        print(indeg)
+        queue = deque()
+
+        for c, ideg in enumerate(indeg):
+            if ideg == 0:
+                queue.append(c)
+
+        processed = 0
+        while queue:
+            cur = queue.popleft()
+            processed += 1
+            for course in premap[cur]:
+                indeg[course] -= 1
+                if indeg[course] == 0:
+                    queue.append(course)
+        if processed == numCourses:
+            return True
+        else:
+            return False
+            
