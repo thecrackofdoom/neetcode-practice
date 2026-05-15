00c8effbff2f9e056bab50a7682d04f017871110
tmng <114478287+thecrackofdoom@users.noreply.github.com>
2026-05-15 14:58:32 +0800

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        graphmap = {i: [] for i in range(n)}
+        for e1, e2 in edges:
+            graphmap[e1].append(e2)
+            graphmap[e2].append(e1)
+
+        visited = set()
+        def dfs(cur, con):
+            print(visited, cur, con)
+            if cur != con and cur in visited:
+                return False
+            
+            for node in graphmap[cur]:
+                visited.add(node)
+                dfs(node, cur)
+                
+            return True
+        
+        for e1, e2 in edges:
+            
+            if not dfs(e1, e2):
+                return False
+        
+        return True
+
+            
