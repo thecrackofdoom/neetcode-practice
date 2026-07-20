bb9635b720ae71d3724806bbcac37631065ca7a6
tmng <114478287+thecrackofdoom@users.noreply.github.com>
2026-07-20 22:28:35 +0800

    Add: count-paths - submission-8

---
+++ Data Structures & Algorithms/count-paths/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        map = [[0] * n for i in range(m)]
+        
+        def path(row, col):
+            if row == 0 or col == 0:
+                map[row][col] = 1
+                return 1
+            map[row][col] = path(row -1, col) + path(row, col - 1)
+
+            return map[row][col]
+        
+        
+        return path(m-1, n-1)
+        
