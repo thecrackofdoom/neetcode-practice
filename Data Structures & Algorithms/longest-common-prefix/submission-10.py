096070f10bc01310e3ddcfe377654a42b0297652
tmng <114478287+thecrackofdoom@users.noreply.github.com>
2026-08-15 22:46:49 +0800

    Add: longest-common-prefix - submission-10

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-10.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        ptr = 0
+        if not strs:
+            return ""
+        ans = None
+        while ptr < min([len(x) for x in strs]):
+            curr = strs[0][ptr]
+            for string in strs:
+                if string[ptr] != curr:
+                    return string[:ptr]
+                ans = string[:ptr+1]
+            ptr += 1
+        return ans
